6618357b5873f2ebf7eb856be0b987e0fcd88269
Ваше Имя <you@example.com>
2018-11-19 13:35:56 +0500

    noy drebezg

--- 02button/main.c
+++ 02button/main.c
@@ -3,13 +3,23 @@
 #include "timex.h"
 #include "periph/gpio.h"
 
+xtimer_ticks32_t last_wakeup;
+
 // Обработчик прерывания по нажатию кнопки
 void btn_handler(void *arg)
 {
+//last_wakeup = xtimer_now();
   // Прием аргументов, передаваемых из главного потока.
   (void)arg;
+
   // Переключение состояния пина PC8
   gpio_toggle(GPIO_PIN(PORT_C, 8));
+ //gpio_toggle(GPIO_PIN(PORT_C, 8));
+
+xtimer_sleep(1);
+
+//gpio_toggle(GPIO_PIN(PORT_C, 8));
+
 }
 
 
@@ -19,11 +29,24 @@ int main(void)
   // GPIO_RISING - прерывание срабатывает по фронту
   // btn_handler - имя функции обработчика прерывания
   // NULL - ничего не передаем в аргументах
-  gpio_init_int(GPIO_PIN(PORT_A, 0), GPIO_IN, GPIO_RISING, btn_handler, NULL);
-  // Инициализация пина PC8 на выход
-	gpio_init(GPIO_PIN(PORT_C, 8), GPIO_OUT);
+ 
+ 
 
-  while(1){}
+
+
+gpio_init_int(GPIO_PIN(PORT_A, 0), GPIO_IN, GPIO_RISING, btn_handler, NULL);
+
+
+
+while(1){
+
+
+}	
+gpio_init(GPIO_PIN(PORT_C, 9), GPIO_OUT);
+gpio_init(GPIO_PIN(PORT_C, 8), GPIO_OUT);
+
+
+
   return 0;
 }
 
